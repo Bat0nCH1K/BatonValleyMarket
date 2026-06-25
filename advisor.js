830e890f3072e595214fb6f65cb3ce67bdc85be4
BatonCHIK <anas2013tatar@gmail.com>
2026-06-25 21:19:05 +0300

    Update advisor.js

--- advisor.js
+++ advisor.js
@@ -1,6 +1,6 @@
 // Wasteland Market Terminal — advisor.js (советник OWL)
 let advisorHistory = [];
-
+ 
 function getKey() { 
     return atob('c2stb3ItdjEtODM0OGE1YmYzOGRiMTYyZGNmZGQxNTRkYjExYmEwYTA2NGY2YmRiNDg5M2Y0ZjMwMDNlNzY3ZTcyOTkxNDY3Ng==');
 }
